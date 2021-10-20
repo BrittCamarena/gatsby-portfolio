9b244ba52ebfad93349981c47f1d359ece701fb7
Brittany Camarena <brwardrip@gmail.com>
2021-10-19 23:11:42 -0700

    Tweaks and changes

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d2fe745060144ebd9a3e.js"
+    "url": "webpack-runtime-74db26673551756cc570.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "65e83470e2e632c8dea3e356d9b94868"
+    "revision": "c299d147c230b3621ff5fea870e2372b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "4d374ce287cff977521aa66830a8d84b"
+    "revision": "4f7464705258941a7b3cb53ba18c3c73"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
